8f70f49525a09b77b44eebb49cf5dc3c2ef64d00
Tyson Lawrie <twlawrie@us.ibm.com>
2018-10-30 18:12:26 +0200

    Updated with latest docker container and services

--- src/main/java/net/boomerangplatform/configuration/KubeConfiguration.java
+++ src/main/java/net/boomerangplatform/configuration/KubeConfiguration.java
@@ -1,13 +1,13 @@
 package net.boomerangplatform.configuration;
 
-import java.util.concurrent.TimeUnit;
+import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.kubernetes.client.ApiClient;
-import io.kubernetes.client.auth.ApiKeyAuth;
+import io.kubernetes.client.util.Config;
 
 @Configuration
 public class KubeConfiguration {
@@ -23,17 +23,29 @@ public class KubeConfiguration {
 
 	@Bean
 	public ApiClient connectToKube() {
-		ApiClient defaultClient = io.kubernetes.client.Configuration.getDefaultApiClient().setVerifyingSsl(false).setDebugging(kubeApiDebug.isEmpty() ? false : Boolean.valueOf(kubeApiDebug));
-		defaultClient.setBasePath(kubeApiBasePath);
-		defaultClient.getHttpClient().setReadTimeout(60, TimeUnit.SECONDS); //added for watcher to not timeout
-
-		ApiKeyAuth apiKeyAuth = (ApiKeyAuth) defaultClient.getAuthentication("BearerToken");
-		apiKeyAuth.setApiKey(kubeApiToken);
-		apiKeyAuth.setApiKeyPrefix("Bearer");
-		
+//		ApiClient defaultClient = null;
+//		if (kubeApiBasePath.equals("")) {
+		ApiClient defaultClient;
+		try {
+			defaultClient = Config.defaultClient();
+			io.kubernetes.client.Configuration.setDefaultApiClient(defaultClient);
+			return defaultClient;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+//		} else {
+//			defaultClient = io.kubernetes.client.Configuration.getDefaultApiClient().setVerifyingSsl(false).setDebugging(kubeApiDebug.isEmpty() ? false : Boolean.valueOf(kubeApiDebug));
+//			defaultClient.setBasePath(kubeApiBasePath);
+//			defaultClient.getHttpClient().setReadTimeout(60, TimeUnit.SECONDS); //added for watcher to not timeout
+//	
+//			ApiKeyAuth apiKeyAuth = (ApiKeyAuth) defaultClient.getAuthentication("BearerToken");
+//			apiKeyAuth.setApiKey(kubeApiToken);
+//			apiKeyAuth.setApiKeyPrefix("Bearer");
+//		}
 //		ApiClient defaultClient = Config.fromToken(kubeApiBasePath, kubeApiToken, false).setVerifyingSsl(false).setDebugging(kubeApiDebug.isEmpty() ? false : Boolean.valueOf(kubeApiDebug));
 //		defaultClient.getHttpClient().setReadTimeout(60, TimeUnit.SECONDS);
 		
-		return defaultClient;
+		return null;
 	}
 }
