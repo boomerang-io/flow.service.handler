b8f7d2ce2271e0e5e52c453084f1e641b1f6f4e5
Tyson Lawrie <twlawrie@us.ibm.com>
2019-07-30 17:22:45 -0500

    Added Logging Statement

--- src/main/java/net/boomerangplatform/service/CICDControllerServiceImpl.java
+++ src/main/java/net/boomerangplatform/service/CICDControllerServiceImpl.java
@@ -73,6 +73,7 @@ public class CICDControllerServiceImpl implements ControllerService {
 			e.printStackTrace();
 			response.setCode("1");
 			response.setMessage(e.toString());
+			System.out.println("DEBUG::Task Is Being Set as Failed");
 		} finally {
 //			response.setOutput(kubeService.getTaskOutPutConfigMapData(task.getWorkflowId(), task.getWorkflowActivityId(), task.getTaskId(), task.getTaskName()));
 			kubeService.deleteConfigMap(task.getWorkflowId(), task.getWorkflowActivityId(), task.getTaskId());
