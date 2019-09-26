6c811d1705803bc370e28a818f556f466ad91474
Tyson Lawrie <twlawrie@us.ibm.com>
2019-09-27 09:05:18 +1000

    :fire: removed argo code and archived into a tag

--- src/main/java/net/boomerangplatform/service/ArgoService.java
+++
@@ -1,9 +0,0 @@
-package net.boomerangplatform.service;
-
-import net.boomerangplatform.model.argo.Workflow;
-
-public interface ArgoService {
-
-	Workflow getWorkflow(String name);
-	Object createWorkflow(Workflow workflow);
-}
