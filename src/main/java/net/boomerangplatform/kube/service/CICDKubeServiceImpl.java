712c47449700e20398de65f599149f101546d715
Tyson Lawrie <twlawrie@us.ibm.com>
2020-03-19 10:02:22 +1100

    :ok_hand: updated plt-3915 based on peer review, adjustment of toleration and selector, and impl in abstract for both CICD and Flow

--- src/main/java/net/boomerangplatform/kube/service/CICDKubeServiceImpl.java
+++ src/main/java/net/boomerangplatform/kube/service/CICDKubeServiceImpl.java
@@ -31,7 +31,6 @@ import io.kubernetes.client.models.V1PodSpec;
 import io.kubernetes.client.models.V1PodTemplateSpec;
 import io.kubernetes.client.models.V1ProjectedVolumeSource;
 import io.kubernetes.client.models.V1ResourceRequirements;
-import io.kubernetes.client.models.V1Toleration;
 import io.kubernetes.client.models.V1Volume;
 import io.kubernetes.client.models.V1VolumeProjection;
 
@@ -171,13 +170,7 @@ public class CICDKubeServiceImpl extends AbstractKubeServiceImpl {
     podSpec.containers(containerList);
     
     if (kubeWorkerDedicatedNodes) {
-	    V1Toleration nodeTolerationItem = new V1Toleration();
-	    nodeTolerationItem.key("dedicated");
-	    nodeTolerationItem.value("bmrg-cicd-worker");
-	    nodeTolerationItem.effect("NoSchedule");
-	    nodeTolerationItem.operator("Equal");
-	    podSpec.addTolerationsItem(nodeTolerationItem);
-	    podSpec.putNodeSelectorItem("node-role.kubernetes.io/bmrg-cicd-worker", "true");
+    	getTolerationAndSelector(podSpec);
     }
 
     if (!kubeWorkerHostAliases.isEmpty()) {
