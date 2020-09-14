348c1eb7355cd0238e71f001913a3df783a36823
Tyson Lawrie <twlawrie@us.ibm.com>
2020-09-15 08:51:11 +1000

    Adjustments after review

--- src/main/java/net/boomerangplatform/kube/service/AbstractKubeServiceImpl.java
+++ src/main/java/net/boomerangplatform/kube/service/AbstractKubeServiceImpl.java
@@ -181,9 +181,9 @@ public abstract class AbstractKubeServiceImpl implements AbstractKubeService { /
   protected abstract V1ConfigMap createWorkflowConfigMapBody(String workflowName, String workflowId,
       String workflowActivityId, Map<String, String> inputProps);
 
-  public abstract String getPrefixJob();
+  public abstract String getJobPrefix();
 
-  public abstract String getPrefixPVC();
+  public abstract String getPVCPrefix();
   
   @Override
   public V1Job createJob(boolean createLifecycle, String workflowName, String workflowId, String workflowActivityId,  String taskActivityId,
@@ -449,7 +449,7 @@ public abstract class AbstractKubeServiceImpl implements AbstractKubeService { /
     V1ObjectMeta metadata = new V1ObjectMeta();
     metadata.annotations(createAnnotations(workflowName, workflowId, workflowActivityId, null));
     metadata.labels(createLabels(workflowId, workflowActivityId, null));
-    metadata.generateName(getPrefixPVC() + "-");
+    metadata.generateName(getPVCPrefix() + "-");
     body.metadata(metadata);
 
     // Create PVC Spec
