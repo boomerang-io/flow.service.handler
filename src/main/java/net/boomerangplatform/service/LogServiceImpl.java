ab0c7517fcc3f752d87d35f0c65eea8c619e0768
Marcus Roy <marcus.d.roy@gmail.com>
2020-09-24 15:12:13 -0500

    :bug: fixing compilation error

--- src/main/java/net/boomerangplatform/service/LogServiceImpl.java
+++ src/main/java/net/boomerangplatform/service/LogServiceImpl.java
@@ -52,13 +52,13 @@ public class LogServiceImpl implements LogService {
 
   @Autowired
   private AbstractKubeServiceImpl kubeService;
-  
+
   @Value("${kube.worker.logging.host}")
   protected String lokiHost;
-  
+
   @Value("${kube.worker.logging.port}")
   protected String lokiPort;
- 
+
 
   @Override
   public StreamingResponseBody streamLogForTask(HttpServletResponse response, String workflowId,
@@ -72,7 +72,7 @@ public class LogServiceImpl implements LogService {
       } else if ("elastic".equals(loggingType)) {
         return streamLogsFromElastic(taskActivityId);
       } else if ("loki".equals(loggingType)) {
-        return streamLogsFromLoki( workflowId, workflowActivityId, taskId, taskActivityId);
+        return streamLogsFromLoki(workflowId, workflowActivityId, taskId, taskActivityId);
       } else {
         return getDefaultErrorMessage(getMessageUnableToAccessLogs());
       }
@@ -83,92 +83,88 @@ public class LogServiceImpl implements LogService {
   }
 
   // TODO: reduce complexity, refactor method
-  private StreamingResponseBody streamLogsFromLoki(String workflowId,
-      String workflowActivityId, String taskId, String taskActivityId) {
+  private StreamingResponseBody streamLogsFromLoki(String workflowId, String workflowActivityId,
+      String taskId, String taskActivityId) {
 
-    LOGGER.info(
-        "Streaming logs from loki: " + taskActivityId);
+    LOGGER.info("Streaming logs from loki: " + taskActivityId);
 
 
     return outputStream -> {
-  
+
       PrintWriter printWriter = new PrintWriter(outputStream);
-      
-      final String filter =  createLokoFilter(workflowId, workflowActivityId, taskId, taskActivityId);
-      LOGGER.info(
-          "Loki filter: " + filter);
-      
+
+      final String filter =
+          createLokoFilter(workflowId, workflowActivityId, taskId, taskActivityId);
+      LOGGER.info("Loki filter: " + filter);
+
       final String encodedQuery = URLEncoder.encode(filter, StandardCharsets.UTF_8);
       final Integer limit = 5000; // max chunk size supported by Loki
-      final String direction = "forward";  //default backward
+      final String direction = "forward"; // default backward
       final String lokiEndpoint = "http://" + lokiHost + ":" + lokiPort;
-      final String uri =  lokiEndpoint + 
-          "/loki/api/v1/query_range?&limit=" + Integer.toString(limit) +
-          "&direction=" + direction + 
-          "&query=" + encodedQuery;
-          // If no `end` argument is defined, it will be automatically set to `now()` by server 
+      final String uri = lokiEndpoint + "/loki/api/v1/query_range?&limit=" + Integer.toString(limit)
+          + "&direction=" + direction + "&query=" + encodedQuery;
+      // If no `end` argument is defined, it will be automatically set to `now()` by server
 
-      LOGGER.info(
-            "Loki endpoint: " + uri);
-            
+      LOGGER.info("Loki endpoint: " + uri);
+
       String start = "&start=0"; // Thursday, January 1, 1970 12:00:00 AM
       Boolean moreLogsAvailable = Boolean.TRUE; // TODO: create a method instead
       CloseableHttpClient httpClient = HttpClients.createDefault();
-      
+
       try {
-        while(moreLogsAvailable.equals(Boolean.TRUE)){
+        while (moreLogsAvailable.equals(Boolean.TRUE)) {
           HttpGet request = new HttpGet(uri + start);
           JSONObject currentLogBatch;
-            
+
           CloseableHttpResponse response = httpClient.execute(request);
           try {
             HttpEntity entity = response.getEntity();
             if (entity != null) {
 
-              //TODO check if result is in JSON format
+              // TODO check if result is in JSON format
               currentLogBatch = new JSONObject(EntityUtils.toString(entity));
-                  
+
               JSONArray queryResults = currentLogBatch.getJSONObject("data").getJSONArray("result");
               JSONArray logArray;
               String logEntry;
-              
+
               if (queryResults.length() == 0) {
                 printWriter.println(getMessageUnableToAccessLogs());
                 printWriter.flush();
                 printWriter.close();
                 return;
               }
-          
-                
-              if(queryResults.length() > 0){
+
+
+              if (queryResults.length() > 0) {
                 logArray = queryResults.getJSONObject(0).optJSONArray("values");
 
                 int index = 1;
-                if(start.equals("&start=0")) index = 0; //no prior log line to overlap
+                if (start.equals("&start=0"))
+                  index = 0; // no prior log line to overlap
 
-                for(; index < logArray.length(); index++){//print line by line
+                for (; index < logArray.length(); index++) {// print line by line
                   logEntry = logArray.getJSONArray(index).get(1).toString();
-                  printWriter.print(logEntry); //TODO: can I generate multiline payloads?
+                  printWriter.print(logEntry); // TODO: can I generate multiline payloads?
                 }
 
-                if(logArray.length() < limit){ //check if the current iteration is the last one
+                if (logArray.length() < limit) { // check if the current iteration is the last one
                   moreLogsAvailable = Boolean.FALSE;
-                }else{
+                } else {
                   JSONArray lastEntry = logArray.getJSONArray(logArray.length() - 1);
-                  start = "&start=" + lastEntry.get(0).toString();                          
+                  start = "&start=" + lastEntry.get(0).toString();
                 }
-              }else{
+              } else {
                 moreLogsAvailable = Boolean.FALSE;
               }
             } else {
-                printWriter.println(getMessageUnableToAccessLogs());
-                printWriter.flush();
-                printWriter.close();
-                return;
-              }
+              printWriter.println(getMessageUnableToAccessLogs());
+              printWriter.flush();
+              printWriter.close();
+              return;
             }
           } finally {
-              response.close();
+            response.close();
           }
         }
       } finally {
