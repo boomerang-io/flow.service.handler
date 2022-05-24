b00fc05c423dd904c3715e5cb26448c5c7e9f844
amhudson <amhudson@us.ibm.com>
2022-05-24 09:14:00 -0500

    spring boot/ dependency updates

--- src/main/java/io/boomerang/Application.java
+++ src/main/java/io/boomerang/Application.java
@@ -4,13 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.models.OpenAPI;
 
 @EnableAsync
 @SpringBootApplication
 @EnableAutoConfiguration
 public class Application {
 
-  public static void main(String[] args) {    
+  public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
+  }
+
+  @Bean
+  public OpenAPI api() {
+    return new OpenAPI();
   }
 }
